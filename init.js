5d00cb30d935e9cb105866f406029d05dc59b7f5
xtremedmg <xtremedmg@gmail.com>
2014-12-09 11:03:11 +1030

    Replace UX
    Replace UX with Testing

--- init.js
+++ init.js
@@ -1,14 +1,14 @@
 ﻿setTimeout(function(){
-	magic_ux_shit = ux_bullshit()
+	magic_testing_shit = testing_bullshit()
 	var item = document.getElementById("technique");
-	item.textContent = '“' + magic_ux_shit + '”';
+	item.textContent = '“' + magic_testing_shit + '”';
 	
 	var more = document.getElementById("more-info");
 	more.removeAttribute("style");
 	
 	var tweet = document.getElementById("tweet");
-	var technique_encoded = encodeURIComponent(magic_ux_shit + " — via @uxbullshit");
-	var url_encoded = encodeURIComponent('http://uxbullshit.com')
+	var technique_encoded = encodeURIComponent(magic_testing_shit + " — via @TestingBullshit");
+	var url_encoded = encodeURIComponent('http://testingbullshit.com')
 	var tweet_link = 'https://twitter.com/intent/tweet?text=' + technique_encoded + "&url=" + url_encoded;
 	tweet.href = tweet_link
 }, 2000);
