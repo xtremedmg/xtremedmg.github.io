e83f93cbd13423194977c48fe1c59b14d2b91866
xtremedmg <xtremedmg@gmail.com>
2014-12-09 11:07:44 +1030

    Update variables

--- testing_bullshit.js
+++ testing_bullshit.js
@@ -1,17 +1,17 @@
 function testing_bullshit() {
 	var result = [
-		"You should try a " + adjective() +" " + ux_technique() + " with a " + role() + " to " + outcome(),
+		"You should try a " + adjective() +" " + testing_technique() + " with a " + role() + " to " + outcome(),
 		"Go and fire a " + role(),
-		"Think in " + ux_technique() + " as the new " + ux_technique(),
-		company() + " is killing " + ux_technique(),
-		"You should use " + adjective() +" " + ux_technique() + " to " + outcome(),
-		"Don't trust " + ux_people() + " and use a " + adjective() +" " + ux_technique() + " if you need to " + outcome(),
-		"Follow " + ux_people() + " and " + outcome() + " using a " + adjective() +" " + adjective() + " " + ux_technique(),
-	    "To " + outcome() + " try thinking in a " + adjective() +" " + ux_technique(),
-	    "A wise man would use a " + adjective() + " " + adjective() + " " + ux_technique() + " to " + outcome(),
+		"Think in " + testing_technique() + " as the new " + testing_technique(),
+		company() + " is killing " + testing_technique(),
+		"You should use " + adjective() +" " + testing_technique() + " to " + outcome(),
+		"Don't trust " + testing_people() + " and use a " + adjective() +" " + testing_technique() + " if you need to " + outcome(),
+		"Follow " + testing_people() + " and " + outcome() + " using a " + adjective() +" " + adjective() + " " + testing_technique(),
+	    "To " + outcome() + " try thinking in a " + adjective() +" " + testing_technique(),
+	    "A wise man would use a " + adjective() + " " + adjective() + " " + testing_technique() + " to " + outcome(),
 	    "Think outside the box to " + outcome() + " and " + outcome(),
-	    "You can simply " + outcome() + " applying a " + adjective() +" " + ux_technique(),
-	    "Stop using a " + adjective() +' '+ adjective() +" " + ux_technique() + " if you need to " + outcome(),
+	    "You can simply " + outcome() + " applying a " + adjective() +" " + testing_technique(),
+	    "Stop using a " + adjective() +' '+ adjective() +" " + testing_technique() + " if you need to " + outcome(),
  	];
 	return result[Math.floor(Math.random()*result.length)];
 }
