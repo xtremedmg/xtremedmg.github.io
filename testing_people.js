4407a72b2ad4a4a36318010f1d2ae29becc1ab02
xtremedmg <xtremedmg@gmail.com>
2014-12-12 14:48:37 +1030

    Updating to testing people

--- testing_people.js
+++ testing_people.js
@@ -1,16 +1,14 @@
 function testing_people() {
 	return random([
-		"Jakob Nielsen",
-		"Don Norman",
-		"Steve Jobs",
-		"Alan Cooper",
-		"David Carson",
-		"Bill Buxton",
-		"John Ive",
-		"Douglas Adams",
-		"Albert Einstein",
-		"Paul Graham",
-		"Jef Raskin",
-		"Tim Berners-Lee",
+		"Rex Black",
+		"Michael Bolton",
+		"James Bach",
+		"Richard Robinson",
+		"Brian Osman",
+		"Oliver Erlewein",
+		"David Greenlees",
+		"Paul Holland",
+		"Keith Klain",
+		"Randall Rice",
 	]);
 }
