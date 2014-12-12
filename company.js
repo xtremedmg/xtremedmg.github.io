810decf0f5430431c31674cdb05b34ea8f5ba096
xtremedmg <xtremedmg@gmail.com>
2014-12-12 14:10:54 +1030

    Updating testing companies

--- company.js
+++ company.js
@@ -1,19 +1,8 @@
 function company() {
 	return random([
-		"Google",
-		"Microsoft",
-		"Apple",
-		"Amazon",
-		"Twitter",
-		"ThoughtWorks",
-		"Facebook",
-		"Adobe",
-		"Reddit",
-		"Yahoo",
-		"Oracle",
-	    "37 degrees",
-	    "Mailchimp",
-	    "Netflix",
-	    "Dropbox",
+		"ISTQB",
+		"ASTQB",
+		"ANZTB",
+		"IBM",
 	]);
 }
