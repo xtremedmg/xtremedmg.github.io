3d2562936fe58863426f5d3d33b6eb08cf3283ef
xtremedmg <xtremedmg@gmail.com>
2014-12-18 14:09:43 +1030

    Update outcomes

--- outcome.js
+++ outcome.js
@@ -1,27 +1,30 @@
 function outcome() {
 	return random([
-		"increase conversion rates",
+		"increase product quality",
 		"identify pain points",
-		"decrease pain expectations",
-		"benchmark transversal resources",
-		"empower taxonomy transformation",
+		"increase test automation",
+		"benchmark system performance",
 		"increase ROI",
+		"identify long lasting test strategies"
+		"make use of best practices"
 		"change the game plan",
+		"develop a 200 page test plan"
 		"go viral in social media channels",
 		"enhance user experience",
 		"reduce system leverage",
-		"pivot around future markets",
-		"position HCI strategies",
-		"foster new paradigms in research",
-		"avoid assumption reversal",
-		"increase deliverables quality",
-		"create long-lasting experiences",
+		"provide 100% code coverage"
+		"foster new paradigms in testing",
+		"avoid assumption making",
+		"mitigate 100% product risk"
+		"increase documentation quality",
+		"accurately estimate testing effort",
 		"cross-pollinate ideas between underpowered disciplines",
 		"increase systemic innovation and take entrepreneurial action",
 		"encourage jazz-like improvisation within a guiding structure",
 		"create a mindset of creative dissatisfaction to reduce resistance to change",
-		"develop a vision-focused innovation strategy",
-		"unpack competitive self-confidence innovation",
-		"lower combinatorial impairment"
+		"develop a automate-everything innovation strategy",
+		"achieve a 100% test case pass rate",
+		"lower combinatorial impairment",
+		"achieve ultimate GUI atumation god-like status",
 	]);
 }
